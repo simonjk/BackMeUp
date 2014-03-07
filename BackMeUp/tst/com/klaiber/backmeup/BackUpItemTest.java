3e3bdfbfebf0478b912b3fc26be4e8bdf6dd2287
xsf130 <xsf130@DEFRALT007606.de.db.com>
2014-03-07 17:16:24 +0100

    Changing Base 36 functionality to Java Standard

--- BackMeUp/tst/com/klaiber/backmeup/BackUpItemTest.java
+++ BackMeUp/tst/com/klaiber/backmeup/BackUpItemTest.java
@@ -2,13 +2,17 @@ package com.klaiber.backmeup;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 public class BackUpItemTest {
 
 	@Test
 	public void testGenerateHashForFile() {
-		assertEquals("Hash not correct","5RPUBH26UT8GC80WGWWCKC080G0KOWG0",BackupItem.generateHashForFile("testdata/test1_1/Download-icon.png"));
+		assertEquals("Hash not correct","5rpubh26ut5nshs3psmm9m1h257q2a7nl32ani1fs50rfao0as",BackupItem.generateHashForFile("testdata/test1_1/Download-icon.png"));
 	}
 
+	
+	
 }
